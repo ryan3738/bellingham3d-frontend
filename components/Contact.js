c6835fc321c959236b2c946ee6e39ab6c67fef62
Ryan <ryan3738@gmail.com>
2021-02-23 11:55:28 -0800

    smaller cart header

--- components/Contact.js
+++ components/Contact.js
@@ -6,56 +6,55 @@ import { siteData } from '../public/site-data';
 export default function Contact() {
   return (
     <>
-      <div className="contact-content">
-        <h2>This is the contact page</h2>
-        <div id="contact-info">
-          <h2>LOCATION</h2>
-          <p>
-            <span className="medium-emphasis">
-              {`${siteData.address.street} ${siteData.address.street2}`}
-              <br />
-              {`${siteData.address.city}, ${siteData.address.state} ${siteData.address.zipCode}`}
-              <br />
-              <br />
-              <a href="tel:13601234567">{siteData.phoneNumber}</a>
-              <br />
-              <br />
-              <b>
-                <a
-                  href={siteData.address.directions}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  Get Directions
-                </a>
-              </b>
-            </span>
-          </p>
-        </div>
+      <h2>This is the contact page</h2>
+      <div id="contact-info">
+        <h2>LOCATION</h2>
+        <p>
+          <span className="medium-emphasis">
+            {`${siteData.address.street} ${siteData.address.street2}`}
+            <br />
+            {`${siteData.address.city}, ${siteData.address.state} ${siteData.address.zipCode}`}
+            <br />
+            <br />
+            <a href="tel:13601234567">{siteData.phoneNumber}</a>
+            <br />
+            <br />
+            <b>
+              <a
+                href={siteData.address.directions}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Get Directions
+              </a>
+            </b>
+          </span>
+        </p>
+      </div>
 
-        <div className="social-media-container">
-          <h3>Sign up for our newsletter to receive updates</h3>
-          <Button buttonText="SIGN UP" buttonLink="" />
-          <br />
-          <br />
-          <p>We respect your privacy</p>
-          <h3>For specials and event info follow us on social media</h3>
-          <div className="social-media-contents">
-            <Social
-              href="https://www.instagram.com/"
-              img="/images/instagram-logo.png"
-              alt="instagram"
-              color="--color-instagram"
-            />
-            <Social
-              href="http://facebook.com/"
-              img="/images/facebook-logo.png"
-              alt="facebook"
-              color="--color-facebook"
-            />
-          </div>
+      <div className="social-media-container">
+        <h3>Sign up for our newsletter to receive updates</h3>
+        <Button buttonText="SIGN UP" buttonLink="" />
+        <br />
+        <br />
+        <p>We respect your privacy</p>
+        <h3>For project updates follow us on social media</h3>
+        <div className="social-media-contents">
+          <Social
+            href="https://www.instagram.com/"
+            img="/images/instagram-logo.png"
+            alt="instagram"
+            color="--color-instagram"
+          />
+          <Social
+            href="http://facebook.com/"
+            img="/images/facebook-logo.png"
+            alt="facebook"
+            color="--color-facebook"
+          />
         </div>
       </div>
+
       <style jsx>{`
         /* .contact-container {
           display: flex;
@@ -65,12 +64,6 @@ export default function Contact() {
           padding: 3rem 0 0;
         } */
 
-        .contact-content {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          padding: 2rem 1rem;
-        }
         .social-media-container {
           display: flex;
           flex-direction: column;
--- components/styles/Supreme.js
+++ components/styles/Supreme.js
@@ -5,9 +5,9 @@ const Supreme = styled.h3`
   color: white;
   display: inline-block;
   padding: 4px 5px;
-  transform: skew(-3deg);
+  /* transform: skew(-3deg); */
   margin: 0;
-  font-size: 4rem;
+  font-size: 1.5rem;
 `;
 
 export default Supreme;
