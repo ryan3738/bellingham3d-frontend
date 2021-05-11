d762409fcae284f26066cdb875dfabac2626b966
Ryan <ryan3738@gmail.com>
2021-05-11 12:32:48 -0700

    remove default address display in account

--- components/Account.js
+++ components/Account.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import DisplayAddress from './Addresses/DisplayAddress';
 import Button from './Button';
 import SignOut from './SignOut';
 import ContainerGrid from './styles/ContainerGrid';
@@ -7,29 +8,15 @@ import { useUser } from './User';
 export default function Account(props) {
   const user = useUser();
   if (!user) return null;
-  // console.log(me);
+  // console.log(user);
   return (
     <ContainerGrid>
-      <h1>Dashboard</h1>
+      <h1>Account Dashboard</h1>
       <h2>{user?.name}</h2>
       <h3>{user.email}</h3>
 
-      {user.defaultAddress && (
-        <div>
-          <h4>Default Address</h4>
-          <p>
-            {`${user.defaultAddress.firstName} ${user.defaultAddress.lastName}`}
-          </p>
-          <p>{user.defaultAddress.company}</p>
-          <p>{user.defaultAddress.address1}</p>
-          <p>{user.defaultAddress.address2}</p>
-          <p>
-            {user.defaultAddress.city}, {user.defaultAddress.country}
-          </p>
-          <p>{user.defaultAddress.zip}</p>
-          <p>{user.defaultAddress.phone}</p>
-        </div>
-      )}
+      <h4>Default Shipping Address</h4>
+      {/* <DisplayAddress id={user?.defaultShipping.id} /> */}
 
       <Button internalLink="/account/addresses">Manage Addresses</Button>
       <Button internalLink="/account/orderhistory">Order History</Button>
