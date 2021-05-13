520744817d97b4ec00b86e2f30601de1cbc6d48d
Ryan <ryan3738@gmail.com>
2021-05-13 08:54:10 -0700

    remove address query pass address object in as props

--- components/Addresses/DisplayAddress.js
+++ components/Addresses/DisplayAddress.js
@@ -1,43 +1,10 @@
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
-import { string } from 'prop-types';
+import { object } from 'prop-types';
 
-const SINGLE_ADDRESS_QUERY = gql`
-  query SINGLE_ADDRESS_QUERY($id: ID!) {
-    CustomerAddress(where: { id: $id }) {
-      id
-      firstName
-      lastName
-      company
-      address1
-      address2
-      city
-      region
-      country
-      zip
-      phone
-      isDefaultShipping {
-        id
-      }
-    }
-  }
-`;
-
-export default function DisplayAddress({ id }) {
-  // 1. We need to get the existing product
-  const { data, error, loading } = useQuery(SINGLE_ADDRESS_QUERY, {
-    variables: { id },
-  });
-  if (!data) return null;
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
-  const address = data.CustomerAddress;
-  console.log('Display Address', data);
-
+export default function DisplayAddress({ address }) {
   return (
     <>
       {address && (
-        <p>
+        <>
           {address.isDefaultShipping?.id && <strong>Default Address</strong>}
           <p>{`${address.firstName} ${address.lastName}`}</p>
           <p>{address.company}</p>
@@ -50,12 +17,13 @@ export default function DisplayAddress({ id }) {
             {address.country} {address.zip}
           </p>
           <p>{address.phone}</p>
-        </p>
+        </>
       )}
+      {/* {!address && <CreateAddress />} */}
     </>
   );
 }
 
 DisplayAddress.propTypes = {
-  id: string,
+  address: object,
 };
