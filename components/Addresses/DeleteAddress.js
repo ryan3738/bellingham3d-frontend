d238b3878ac4b9904fffc96c12975adcefae7609
Ryan <ryan3738@gmail.com>
2021-05-13 08:56:16 -0700

    change import

--- components/Addresses/DeleteAddress.js
+++ components/Addresses/DeleteAddress.js
@@ -1,6 +1,6 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
-import { USER_ADDRESSES_QUERY } from '.';
+import { USER_ADDRESSES_QUERY } from '../../queries/getUserAddresses';
 
 const DELETE_ADDRESS_MUTATION = gql`
   mutation DELETE_ADDRESS_MUTATION($id: ID!) {
