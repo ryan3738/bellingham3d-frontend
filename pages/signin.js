9290c25eff45fd8427703435250239b1e9c508ff
Ryan <ryan3738@gmail.com>
2021-05-17 08:57:08 -0700

    component cleanup

--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,4 @@
-import Link from 'next/link';
-import RequestReset from '../components/RequestReset';
 import SignIn from '../components/SignIn';
-import ContainerGrid from '../components/styles/ContainerGrid';
 
 export default function SignInPage() {
   return (
--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,4 @@
-import RequestReset from '../components/RequestReset';
-import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
-import ContainerGrid from '../components/styles/ContainerGrid';
 
 export default function SignUpPage() {
   return (
