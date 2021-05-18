8b3ad417e1b2416913702c78dcea8df2be357cf8
Ryan <ryan3738@gmail.com>
2021-05-18 15:47:12 -0700

    move single product query to file

---
+++ queries/getSingleProduct.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+
+export const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      price
+      description
+      variants {
+        id
+        name
+        variantType {
+          id
+          name
+        }
+      }
+      image {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
