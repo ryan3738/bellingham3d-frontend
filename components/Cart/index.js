d307563955e4506cef7ca6886e25bfd8766dcc5a
Ryan <ryan3738@gmail.com>
2021-09-02 10:06:03 -0700

    udpate cart modal

--- components/Cart/index.js
+++ components/Cart/index.js
@@ -21,56 +21,57 @@ export default function Cart() {
   const { cartOpen, closeCart } = useCart();
   if (!me) return null;
 
-  // useEffect(() => {
-  //   cartShippingAddress(me.defaultShipping);
-  //   console.log('Default Shipping', me.defaultShipping);
-  //   console.log('cartShippingAddress', cartShippingAddress());
-  // }, [me.defaultShipping]);
-
   const shippingRequired = me.cart.some(
     (cartItem) => cartItem.product.inventoryItem.requiresShipping === true
   );
-  // console.log('Is shipping required?', shippingRequired);
-  // console.log('Cart', me.cart);
+
   return (
     <CartStyles open={cartOpen}>
-      <header>
-        <Supreme>{me.name}'s Cart</Supreme>
-        <ButtonIconStyles onClick={closeCart}>
-          <IconContext.Provider value={{ size: '60px' }}>
-            <FaWindowClose />
-          </IconContext.Provider>
-        </ButtonIconStyles>
-      </header>
+      <div className="cart-content">
+        <header>
+          <Supreme>{me.name}'s Cart</Supreme>
+          <ButtonIconStyles onClick={closeCart}>
+            <IconContext.Provider value={{ size: '60px' }}>
+              <FaWindowClose />
+            </IconContext.Provider>
+          </ButtonIconStyles>
+        </header>
 
-      {me.cart.length === 0 ? (
-        <div>
-          <h3>Your Cart is Empty</h3>
-          <SeeAllProducts />
-        </div>
-      ) : (
-        <>
+        {me.cart.length === 0 ? (
           <div>
-            {me.cart.map((cartItem) => (
-              <CartItem key={cartItem.id} cartItem={cartItem} />
-            ))}
+            <h3>Your Cart is Empty</h3>
             <SeeAllProducts />
           </div>
-          <footer>
-            {shippingRequired ? (
-              <>
-                <p>Subtotal: {formatMoney(calcTotalPrice(me.cart))}</p>
-                <CheckingOut />
-              </>
-            ) : (
-              <>
-                <p>Subtotal: {formatMoney(calcTotalPrice(me.cart))}</p>
-                <Checkout />
-              </>
-            )}
-          </footer>
-        </>
-      )}
+        ) : (
+          <>
+            <div>
+              {me.cart.map((cartItem) => (
+                <CartItem key={cartItem.id} cartItem={cartItem} />
+              ))}
+              <SeeAllProducts />
+            </div>
+            <footer>
+              {shippingRequired ? (
+                <>
+                  <p>Subtotal: {formatMoney(calcTotalPrice(me.cart))}</p>
+                  <CheckingOut />
+                </>
+              ) : (
+                <>
+                  <p>Subtotal: {formatMoney(calcTotalPrice(me.cart))}</p>
+                  <Checkout />
+                </>
+              )}
+            </footer>
+          </>
+        )}
+      </div>
+      <button
+        type="button"
+        className="overlay"
+        onClick={closeCart}
+        aria-label="close cart button"
+      />
     </CartStyles>
   );
 }
