c498d3411863773ffb77258a0eebcd920a9d8374
Ryan <ryan3738@gmail.com>
2021-09-17 14:46:02 -0700

    move product search query

--- components/Search.js
+++ components/Search.js
@@ -1,33 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useLazyQuery } from '@apollo/client';
 import { resetIdCounter, useCombobox } from 'downshift';
-import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { siteData } from '../public/site-data';
+import { SEARCH_PRODUCTS_QUERY } from '../queries/searchProducts';
 import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
-
-const SEARCH_PRODUCTS_QUERY = gql`
-  query SEARCH_PRODUCTS_QUERY($searchTerm: String!) {
-    # Rename property name of query. Good if you want to have mutliple queries of the same property.
-    searchTerms: products(
-      where: {
-        OR: [
-          { name: { contains: $searchTerm } }
-          { description: { contains: $searchTerm } }
-        ]
-      }
-    ) {
-      id
-      name
-      images {
-        image {
-          publicUrlTransformed(transformation: { width: "120" })
-        }
-      }
-    }
-  }
-`;
 
 export default function Search() {
   const router = useRouter();
@@ -82,14 +61,18 @@ export default function Search() {
       <DropDown {...getMenuProps()}>
         {isOpen &&
           items.map((item, index) => (
-            <Link href={`/product/${item.id}`}>
+            <Link href={`/product/${item.id}`} key={index}>
               <DropDownItem
                 key={item.id}
                 {...getItemProps({ item })}
                 highlighted={index === highlightedIndex}
               >
                 <img
-                  src={item?.images[0]?.image.publicUrlTransformed}
+                  src={
+                    item.images[0]
+                      ? item?.images[0]?.image?.publicUrlTransformed
+                      : siteData.placeholderImage.small.src
+                  }
                   alt={item.name}
                   width="50"
                 />
