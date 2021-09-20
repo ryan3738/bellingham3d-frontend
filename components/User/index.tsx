e27d2e2f65cfa2473c854438e202a99cbe7309c6
Ryan <ryan3738@gmail.com>
2021-09-20 13:37:20 -0700

    add User type to return of function

--- components/User/index.tsx
+++ components/User/index.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from '@apollo/client';
 import { CURRENT_USER_QUERY } from '../../queries/getUser';
+import { User } from '../../types';
 
-export function useUser() {
+export function useUser(): User {
   const { data } = useQuery(CURRENT_USER_QUERY);
   return data?.authenticatedItem;
 }
