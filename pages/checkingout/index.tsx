046bf02e0399eed7d450da56f4eb4be0a18bc9a4
Ryan <ryan3738@gmail.com>
2021-09-19 21:16:47 -0700

    .js to .tsx and add types

--- pages/checkingout/index.tsx
+++ pages/checkingout/index.tsx
@@ -1,6 +1,6 @@
 import CheckingOut from '../../components/CheckingOut';
 
-export default function CheckingoutPage() {
+export default function CheckingoutPage(): JSX.Element {
   return (
     <div>
       <CheckingOut />
