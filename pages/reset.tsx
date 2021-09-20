9eeb1373239dccdc0478fc35ac3b7e9a42832e5d
Ryan <ryan3738@gmail.com>
2021-09-19 20:42:35 -0700

    request reset from .js to .tsx. Add typescript types

--- pages/reset.tsx
+++ pages/reset.tsx
@@ -1,8 +1,15 @@
 import RequestReset from '../components/User/RequestReset';
 import Reset from '../components/User/Reset';
 
-export default function ResetPage({ query }) {
+export default function ResetPage({
+  query,
+}: {
+  query: {
+    token: string;
+  };
+}): JSX.Element {
   if (!query?.token) {
+    console.log('query', query);
     return (
       <div>
         <p>Sorry you must supply a token</p>
