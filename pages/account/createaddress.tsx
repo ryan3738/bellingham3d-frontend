d97150d849299782e13c4468863571b754286e05
Ryan <ryan3738@gmail.com>
2021-09-21 15:42:11 -0700

    remove unused page

--- pages/account/createaddress.tsx
+++
@@ -1,9 +0,0 @@
-import CreateAddress from '../../components/Addresses/CreateAddress';
-
-export default function CreateAddressPage(): JSX.Element {
-  return (
-    <div>
-      <CreateAddress />
-    </div>
-  );
-}
