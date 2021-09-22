5f0fff3c0d7771727d0e86f45d56533ea78e2a80
Ryan <ryan3738@gmail.com>
2021-09-22 09:44:41 -0700

    add interface

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,5 +1,25 @@
-const formatDate = (dateString) => {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+interface DateTimeFormatOptions {
+  localeMatcher?: 'best fit' | 'lookup';
+  weekday?: 'long' | 'short' | 'narrow';
+  era?: 'long' | 'short' | 'narrow';
+  year?: 'numeric' | '2-digit';
+  month?: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow';
+  day?: 'numeric' | '2-digit';
+  hour?: 'numeric' | '2-digit';
+  minute?: 'numeric' | '2-digit';
+  second?: 'numeric' | '2-digit';
+  timeZoneName?: 'long' | 'short';
+  formatMatcher?: 'best fit' | 'basic';
+  hour12?: boolean;
+  timeZone?: string;
+}
+
+const formatDate = (dateString: string): string => {
+  const options: DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
   return new Date(dateString).toLocaleDateString(undefined, options);
 };
 
