9338c72be2be6239c4da5b862a624901ef49c0da
Ryan <ryan3738@gmail.com>
2021-09-27 15:51:58 -0700

    add types for add to cart mutation

--- components/Cart/AddToCart.tsx
+++ components/Cart/AddToCart.tsx
@@ -3,6 +3,10 @@ import gql from 'graphql-tag';
 import { ButtonStyles } from '../styles/StateStyles';
 import { CURRENT_USER_QUERY } from '../../queries/getUser';
 import { ProductType, Variant } from '../../types/types';
+import {
+  ADD_TO_CART_MUTATION,
+  ADD_TO_CART_MUTATIONVariables as ADD_TO_CART_MUTATION_VARIABLES,
+} from '../../types/generated/ADD_TO_CART_MUTATION';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($id: ID!, $variantIds: [ID]) {
@@ -18,7 +22,10 @@ type AppProps = {
 };
 
 export default function AddToCart({ id, variantIds }: AppProps): JSX.Element {
-  const [addToCart, { loading }] = useMutation(ADD_TO_CART_MUTATION, {
+  const [addToCart, { loading }] = useMutation<
+    ADD_TO_CART_MUTATION,
+    ADD_TO_CART_MUTATION_VARIABLES
+  >(ADD_TO_CART_MUTATION, {
     variables: { id, variantIds },
     refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
