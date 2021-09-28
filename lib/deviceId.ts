4a5368bcb17b13dbea43eeea7af810580ca89af3
Ryan <ryan3738@gmail.com>
2021-09-27 21:30:25 -0700

    create device id module

--- lib/deviceId.ts
+++ lib/deviceId.ts
@@ -1,10 +1,24 @@
 import { v4 as uuidv4 } from 'uuid';
+import { getLocalStorage, updateLocalStorage } from './localStorage';
+
+const key = 'device-id';
+
+const createDeviceId = (): string => {
+  return uuidv4();
+};
 
 const getDeviceId = (): string => {
-  const myuuid = uuidv4();
-  console.log(`Your UUID is: ${myuuid}`);
-
-  return uuidv4();
+  // const myuuid = uuidv4();
+  const deviceId = getLocalStorage(key);
+  // console.log(`Your UUID is: ${deviceId}`);
+  if (deviceId) {
+    return deviceId;
+  }
+  if (!deviceId) {
+    const newDeviceId = createDeviceId();
+    updateLocalStorage({ key, value: newDeviceId });
+    return getDeviceId();
+  }
 };
 
 export { getDeviceId };
