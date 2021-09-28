b90c0fc5d0a6f93cff17466a1d656665f03ada01
Ryan <ryan3738@gmail.com>
2021-09-27 22:57:30 -0700

    change device-id to deviceId

--- lib/deviceId.ts
+++ lib/deviceId.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { getLocalStorage, updateLocalStorage } from './localStorage';
 
-const key = 'device-id';
+const key = 'deviceId';
 
 const createDeviceId = (): string => {
   return uuidv4();
