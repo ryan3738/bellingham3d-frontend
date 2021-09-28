fa6434ea999881fe10061748c01cd1c67f9bd6e2
Ryan <ryan3738@gmail.com>
2021-09-27 17:33:26 -0700

    format money tests

--- tests/formatMoney.test.ts
+++ tests/formatMoney.test.ts
@@ -4,8 +4,9 @@ test('No value entered returns "$0"', () => {
   expect(formatMoney()).toBe('$0');
 });
 
-test('0 entered returns "$0"', () => {
+test('0 or null entered returns "$0"', () => {
   expect(formatMoney(0)).toBe('$0');
+  expect(formatMoney(null)).toBe('$0');
 });
 
 test('3000 entered returns "$30.00"', () => {
@@ -15,19 +16,3 @@ test('3000 entered returns "$30.00"', () => {
 test('30 entered returns "$.30"', () => {
   expect(formatMoney(30)).toBe('$0.30');
 });
-
-test('If a non neumeric value is entered', () => {
-  expect(formatMoney('acd')).toBe('$0.30');
-});
-
-// test('two plus two', () => {
-//   const value = 2 + 2;
-//   expect(value).toBeGreaterThan(3);
-//   expect(value).toBeGreaterThanOrEqual(3.5);
-//   expect(value).toBeLessThan(5);
-//   expect(value).toBeLessThanOrEqual(4.5);
-
-//   // toBe and toEqual are equivalent for numbers
-//   expect(value).toBe(4);
-//   expect(value).toEqual(4);
-// });
