911626b5f83e54af07a98dbef7b8be83fe2805e8
Ryan <ryan3738@gmail.com>
2021-10-18 12:48:21 -0700

    update formatMoney test

--- tests/formatMoney.test.ts
+++ tests/formatMoney.test.ts
@@ -1,5 +1,21 @@
 import formatMoney from '../lib/formatMoney';
 
+describe('Format money function', () => {
+  it('Works with fractional dollars', () => {
+    expect(formatMoney(1)).toEqual('$0.01');
+    expect(formatMoney(10)).toEqual('$0.10');
+    expect(formatMoney(9)).toEqual('$0.09');
+    expect(formatMoney(40)).toEqual('$0.40');
+    expect(formatMoney(140)).toEqual('$1.40');
+  });
+
+  it("Leaves off cents when it's whole dollars", () => {
+    expect(formatMoney(5000)).toEqual('$50');
+    expect(formatMoney(100)).toEqual('$1');
+    expect(formatMoney(50000000)).toEqual('$500,000');
+  });
+});
+
 test('No value entered returns "$0"', () => {
   expect(formatMoney()).toBe('$0');
 });
