4e0a1f9330b65fb1345e41e7424fcbf2ee1e5050
Ryan <ryan3738@gmail.com>
2021-10-18 13:32:00 -0700

    update product test file

--- tests/product.test.js
+++ tests/product.test.js
@@ -1,9 +1,17 @@
 import { render } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
 import Product from '../components/Products/Product';
+import { fakeItem } from '../lib/testUtils';
+
+const product = fakeItem();
 
 describe('<Product/>', () => {
-  it('Renders out the price tag and title', () => {
-    const { container, debug } = render(<Product />);
+  it('renders out the price tag and title', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
     debug();
   });
 });
