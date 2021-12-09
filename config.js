5da992f53187783e2da6fd7fb3ab88d326249d5d
Ryan <ryan3738@gmail.com>
2021-12-09 15:34:14 -0800

    fix url typo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const localEndpoint = 'http://localhost:3000/api/graphql';
-export const developmentEndpoint = 'https://dev-admin.bellingham3d.com//api/graphql';
-export const previewEndpoint = `https://dev-admin.bellingham3d.com//api/graphql`;
+export const developmentEndpoint = 'https://dev-admin.bellingham3d.com/api/graphql';
+export const previewEndpoint = `https://dev-admin.bellingham3d.com/api/graphql`;
 export const productionEndpoint = `https://admin.bellingham3d.com/api/graphql`;
 export const perPage = 20;
 export const instagramUrl = 'https://www.instagram.com/bellingham.3d/';
