15e6b967875674a54605f35656cad954830a46bc
Ryan <ryan3738@gmail.com>
2021-12-09 09:28:23 -0800

    remove comment in style jsx

--- components/NavBar/NavList.tsx
+++ components/NavBar/NavList.tsx
@@ -87,12 +87,6 @@ const NavList = function ({
       </>
 
       <style jsx>{`
-         {
-          /* .icons {
-          color: var(--navyBlue);
-        } */
-        }
-
         button {
           padding: 0;
           margin: 0;
